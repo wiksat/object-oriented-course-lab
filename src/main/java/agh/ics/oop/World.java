8b355aef3b714e44a7ebfcc15bd4ee03a2f87a40
wiksat <84770801+wiksat@users.noreply.github.com>
2022-10-08 23:44:27 +0200

    commit

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,18 +3,39 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("system wystartował");
-        run(args);
+        Direction[] direction=new Direction[args.length];
+        int licznik=0;
+        for(String argument : args) {
+            Direction kierunek = switch (argument) {
+                case "f" -> Direction.FORWARD;
+                case "b" -> Direction.BACKWARD;
+                case "r" -> Direction.RIGHT;
+                case "l" -> Direction.LEFT;
+                default -> Direction.DEFAULT;
+            };
+            direction[licznik]=kierunek;
+            licznik ++;
+        }
+        run(direction);
         System.out.println("system zakończył działanie");
     }
 
-    public static void run(String[] arguments) {
-        for(String argument : arguments){
+    public static void run(Direction[] args) {
+//        System.out.println("zwierzak idzie do przodu");
+//        int licznik=0;
+        for(Direction argument : args){
+//            if (licznik==0){
+//                System.out.print(argument);
+//            }else{
+//                System.out.print(","+argument);
+//            }
+//            licznik=licznik+1;
             String message = switch (argument) {
-                case "f" -> "Zwierzak idzie do przodu";
-                case "b" -> "Zwierzak idzie do tyłu";
-                case "r" -> "Zwierzak skręca w prawo";
-                case "l" -> "Zwierzak skręca w lewo";
-                default -> "";
+                case FORWARD -> "Zwierzak idzie do przodu";
+                case BACKWARD -> "Zwierzak idzie do tyłu";
+                case RIGHT -> "Zwierzak skręca w prawo";
+                case LEFT -> "Zwierzak skręca w lewo";
+                case DEFAULT -> "";
             };
             System.out.println(message);
         }
