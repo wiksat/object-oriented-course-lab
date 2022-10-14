721f91019a0c1d90a9c4b8590e48665060ee6535
wiksat <84770801+wiksat@users.noreply.github.com>
2022-10-14 15:41:46 +0200

    lab2

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -5,20 +5,15 @@ import java.util.Arrays;
 public class World {
     public static void main(String[] args) {
         System.out.println("system wystartował");
-//        Direction[] direction= assign(args);
-//        run(direction);
+        Direction[] direction= assign(args);
+        run(direction);
+
+//        helo
 //        Vector2d position1 = new Vector2d(1,2);
 //        System.out.println(position1);
 //        Vector2d position2 = new Vector2d(-2,1);
 //        System.out.println(position2);
 //        System.out.println(position1.add(position2));
-
-//        System.out.println(MapDirection.NORTH.toUnitVector());
-//        System.out.println(MapDirection.SOUTH.toUnitVector());
-//        System.out.println(MapDirection.EAST.toUnitVector());
-//        System.out.println(MapDirection.WEST.toUnitVector());
-
-
 
         System.out.println("system zakończył działanie");
     }
